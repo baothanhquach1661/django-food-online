1b33cd0dbce57c5666073309a1d6b1bdcd791ce7
Bao T Quach <baothanhquach1661@gmail.com>
2024-01-11 12:03:06 +0700

    vender & customer registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('register-user/', views.registerUser, name='registerUser')
+    path('register-user/', views.registerUser, name='registerUser'),
+    path('register-vendor/', views.registerVendor, name='registerVendor'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('myAccount/', views.myAccount, name='myAccount'),
+    path('customerDashboard/', views.customerDashboard, name='customerDashboard'),
+    path('vendorDashboard/', views.vendorDashboard, name='vendorDashboard'),
+
 ]
